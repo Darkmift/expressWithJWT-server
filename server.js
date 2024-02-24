c3b28632438d3928572c4985c0bc82e36fd8b8b2
Darkmift <darkmift@gmail.com>
2024-02-24 21:11:36 +0200

    Applied jest

--- server.js
+++ server.js
@@ -1,13 +1,13 @@
 // libraries
-import express from "express";
-import cors from "cors";
-import cookieParser from "cookie-parser";
+import express from 'express';
+import cors from 'cors';
+import cookieParser from 'cookie-parser';
 
 // functions, helpers and constants
-import { PORT } from "./config/config.js";
-import { login } from "./controllers/loginController.js";
-import { calculate } from "./controllers/calculateController.js";
-import { verifyUser } from "./middlewares/verifyUser.js";
+import { PORT } from './config/config.js';
+import { login } from './controllers/loginController.js';
+import { calculate } from './controllers/calculateController.js';
+import { verifyUser } from './middlewares/verifyUser.js';
 
 // server initialization
 const app = express();
@@ -22,12 +22,18 @@ app.use(express.urlencoded({ extended: true }));
 // should be a specific proper whitelist
 app.use(cors());
 
+app.get('/', (req, res) => {
+  res.send('Hello World!');
+});
+
 // the login route will provide authentication with the token from jwt
-app.post("/login", login);
+app.post('/login', login);
 
 // the calculate route is protected by the token from the login route for the next 10 minutes
-app.post("/calculate", verifyUser, calculate);
+app.post('/calculate', verifyUser, calculate);
 
 app.listen(PORT, () => {
-  console.log("Example app listening on port " + PORT);
+  console.log('Example app listening on port ' + PORT);
 });
+
+export default app;
